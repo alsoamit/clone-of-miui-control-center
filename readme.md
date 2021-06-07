94f85f8eff944d474afe56ba8bdd13bee3b0890a
alsoamit <alsoamit@gmail.com>
2021-06-07 18:25:25 +0530

    added readme.md

---
+++ readme.md
@@ -0,0 +1,7 @@
+Clone of MIUI 12's Control Center.
+<br><br>
+
+<h2>PREVIEW</h2>
+<br>
+<img src="preview.png" />
+<img src="preview2.png" />
--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-$(document).ready(()=>{
-    console.log("js is working for you my friend!")
-    $(".c1").click(function(){
-        $(this).toggleClass("clr1");
-        $(this).addClass("press");
-        setInterval(() => {
-            $(this).removeClass("press");
-        }, 300);
-        console.log("clr1");
-    })
-    $(".c2").click(function(){
-        $(this).toggleClass("clr2");
-        $(this).addClass("press");
-        setInterval(() => {
-            $(this).removeClass("press");
-        }, 300);
-        console.log("clr2");
-    })
-    $(".c3").click(function(){
-        $(this).toggleClass("clr3");
-        $(this).addClass("press");
-        setInterval(() => {
-            $(this).removeClass("press");
-        }, 300);
-        console.log("clr3");
-    })
-})
+$(document).ready(() => {
+  $(".c1").click(function () {
+    $(this).toggleClass("clr1");
+    $(this).addClass("press");
+    setInterval(() => {
+      $(this).removeClass("press");
+    }, 300);
+  });
+  $(".c2").click(function () {
+    $(this).toggleClass("clr2");
+    $(this).addClass("press");
+    setInterval(() => {
+      $(this).removeClass("press");
+    }, 300);
+  });
+  $(".c3").click(function () {
+    $(this).toggleClass("clr3");
+    $(this).addClass("press");
+    setInterval(() => {
+      $(this).removeClass("press");
+    }, 300);
+  });
+});
